65b28e245dd8043ed1c6cdddf4287c332ab07338
hayka <hayk.aleksanyan@gmail.com>
2019-04-21 16:38:57 +0400

    encapsulating token info into a seprate class and adjusting the main routines accordingly

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -23,12 +23,30 @@ from Trees import Node
 from Trees import Tree
 
 # constants:
-TOKENS_TO_USE = 250       # number of different tokens to use in the wordle
-STAY_AWAY = 1             # force any two words to stay at least this number of pixels away from each other
+TOKENS_TO_USE = 200       # number of different tokens to use in the wordle
+STAY_AWAY = 2             # force any two words to stay at least this number of pixels away from each other
 FONT_SIZE_MIN = 20        # the smallest font of a word
-FONT_SIZE_MAX = 400       # the largest font of a word
+FONT_SIZE_MAX = 300       # the largest font of a word
 DESIRED_HW_RATIO = 0.618  # height/widht ratio of the canvas
 QUADTREE_MINSIZE = 5      # minimal height-width of the box in quadTree partition
+
+
+
+class Token:
+    """
+        encapsulate the main information on a token into a single class
+        Token here represents a word to be placed on canvas for the final wordle Image
+
+        most of the attributes are filled with functions during processing of the tokens
+    """
+
+    def __init__(self, word, fontSize = 10, drawAngle = 0):
+        self.word = word
+        self.fontSize = fontSize      # an integer
+        self.drawAngle = drawAngle    # an integer, representing the rotation angle of the image; 0 - for NO rotation
+        self.imgSize = None           # tuple of integers (width, height)
+        self.quadTree = None          # the quadTree of the image of this word with the above characteristics
+        self.place = None             # tuple, the coordinate of the upper-left cordner of the token on the final canvas
 
 
 def proposeCanvasSize(quadTrees):
@@ -60,66 +78,55 @@ def normalizeWordSize(tokens, freq, N_of_tokens_to_use, max_size, min_size):
     words = tokens[:N_of_tokens_to_use]
     sizes = freq[:N_of_tokens_to_use]
 
-    # sizes = [int(max_size*x/max(sizes)) for x in sizes]
+    normalTokens = [ ] # the list of Tokens to be returned
+
     # scale the range of sizes to the given range [min_size, max_size]
     a, b = min(sizes), max(sizes)
     sizes = [  int(((max_size - min_size )/(b - a))*( x - a ) + min_size )  for x in sizes ]
 
-    i = 0
-    while i < len(sizes):
-        if sizes[i] < min_size:
-            del sizes[i]
-            del words[i]
-        else:
-            i += 1
+    for i in range(len(sizes)):
+        normalTokens.append( Token( words[i], sizes[i] ) )
+
+    return normalTokens
 
 
 
-    return words, sizes
-
-
-
-def drawWord(word, fsize):
+def drawWord(token):
     """
+      gets an instance of Token class and draws the word it represents
       returns an image of the given word in the given font size
       the image is NOT cropped
     """
 
-    font = ImageFont.truetype("arial.ttf", fsize)
-    w, h = font.getsize(word)
+    font = ImageFont.truetype("arial.ttf", token.fontSize)
+    w, h = font.getsize(token.word)
 
     im = Image.new('RGBA', (w,h), color = None)
     draw = ImageDraw.Draw(im)
-    draw.text((0, 0), word, font = font)
+    draw.text((0, 0), token.word, font = font)
 
     return im
 
 
-def drawOnCanvas(words, sizes, placeInfo ):
+def drawOnCanvas( normalTokens, canvas_size ):
 
-    (c_W,c_H) = placeInfo[0]     # the suggested canvas size, might change here
-    place = placeInfo[1]         # list of tuples, each for each word, showing the coordinate of the upper left corner
-    word_img_size = placeInfo[2] # list of tuples, for each word, showing the size of the word's image
-
-    word_img_path = placeInfo[3]
-
-    assert(len(place) == len(word_img_size))
+    c_W,c_H = canvas_size        # the suggested canvas size, might change here
 
     # there might be some positions of words which fell out of the canvas
     # we first need to go through these exceptions, and expand the canvas and (or) shift the coordinate's origin.
 
     X_min, Y_min = 0, 0
 
-    for i in range(len(place)):
+    for i, token in enumerate(normalTokens):
 
-        if place[i] == None:
+        if token.place == None:
             continue
 
-        if X_min > place[i][0]:
-            X_min = place[i][0]
+        if X_min > token.place[0]:
+            X_min = token.place[0]
 
-        if Y_min > place[i][1]:
-            Y_min = place[i][1]
+        if Y_min > token.place[1]:
+            Y_min = token.place[1]
 
 
     x_shift, y_shift = 0, 0
@@ -129,18 +136,15 @@ def drawOnCanvas(words, sizes, placeInfo ):
         y_shift = -Y_min
 
     X_max , Y_max = 0, 0
-    for i in range( len(place) ):
-        if place[i] == None:
+    for i, token in enumerate(normalTokens):
+        if token.place == None:
             continue
 
-        place[i] = ( place[i][0] + x_shift, place[i][1] + y_shift )
-        if X_max < place[i][0] + word_img_size[i][0]:
-            X_max = place[i][0] + word_img_size[i][0]
-        if Y_max < place[i][1] + word_img_size[i][1]:
-            Y_max = place[i][1] + word_img_size[i][1]
-
-        word_img_path[i] = ( word_img_path[i][0] + x_shift, word_img_path[i][1] )
-
+        token.place = ( token.place[0] + x_shift, token.place[1] + y_shift )
+        if X_max < token.place[0] + token.imgSize[0]:
+            X_max = token.place[0] + token.imgSize[0]
+        if Y_max < token.place[1] + token.imgSize[1]:
+            Y_max = token.place[1] + token.imgSize[1]
 
     if c_W < X_max:
         c_W = X_max
@@ -168,28 +172,29 @@ def drawOnCanvas(words, sizes, placeInfo ):
     # chose the color scheme randomly
     word_colors = color_schemes[ random.randint(0, len(color_schemes) - 1) ]
 
-    max_size = max(sizes)
+    max_size = FONT_SIZE_MAX
 
-    for i in range( len(place) ):
-        if place[i] is None:
-            print('the word <' + words[i] + '> was skipped' )
+    for i, token in enumerate(normalTokens):
+        if token.place == None:
+            print('the word <' + token.word + '> was skipped' )
+            continue
+
+        font1 = ImageFont.truetype("arial.ttf", token.fontSize)
+
+        if token.fontSize >= 0.7*max_size:
+            c = word_colors[-1]
+        elif ((token.fontSize >= 0.5*max_size)and(token.fontSize < 0.7*max_size) ):
+            c = word_colors[-2]
+        elif ((token.fontSize >= 0.35*max_size)and(token.fontSize < 0.5*max_size) ):
+            c = word_colors[-3]
+        elif ((token.fontSize >= 0.15*max_size)and(token.fontSize < 0.35*max_size) ):
+            c = word_colors[-4]
         else:
-            font1 = ImageFont.truetype("arial.ttf", sizes[i])
-
-            if sizes[i] >= 0.7*max_size:
-                c = word_colors[-1]
-            elif ((sizes[i] >= 0.5*max_size)and(sizes[i]<0.7*max_size) ):
-                c = word_colors[-2]
-            elif ((sizes[i] >= 0.35*max_size)and(sizes[i]<0.5*max_size) ):
-                c = word_colors[-3]
-            elif ((sizes[i] >= 0.15*max_size)and(sizes[i]<0.35*max_size) ):
-                c = word_colors[-4]
-            else:
-                c = word_colors[-5]
+            c = word_colors[-5]
 
 
-            dd.text( place[i], words[i], fill = c,  font = font1 )
-            dd_white.text( place[i], words[i], fill = c,  font = font1 )
+        dd.text( token.place, token.word, fill = c,  font = font1 )
+        dd_white.text( token.place, token.word, fill = c,  font = font1 )
 
 
     margin_size = 10
@@ -203,27 +208,25 @@ def drawOnCanvas(words, sizes, placeInfo ):
     return im_canvas_1
 
 
-def createQuadTrees(words, sizes):
+def createQuadTrees(normalTokens):
     """
         given a list of words and their corresponding font-szies, we create QuadTrees for each word
         and return 2 lists, one for the quadtrees and another for sizes of the cropped word-images
     """
-    tree_list, size_list = [], []
 
-    for i in range( len(words) ):
-        im_tmp = drawWord(words[i], sizes[i] )
+    for i, token in enumerate(normalTokens):
+        im_tmp = drawWord(token)
         T = BBox.getQuadTree( im_tmp , QUADTREE_MINSIZE, QUADTREE_MINSIZE )
         T.compress()
-        tree_list.append( T )
-
         im_tmp = im_tmp.crop(im_tmp.getbbox())
-        size_list.append(im_tmp.size)
 
-    return tree_list, size_list
+        token.quadTree = T
+        token.imgSize = im_tmp.size
 
 
 
-def placeWords(words, sizes):
+
+def placeWords(normalTokens):
     """
       gets a list of tokens and their frequencies
       executes the placing strategy and
@@ -234,21 +237,21 @@ def placeWords(words, sizes):
 
     word_img_path = [] # shows the path passed through the spiral before hitting a free space
 
-    print('Number of words equals', len(words), '\n')
+    print('Number of tokens equals', len(normalTokens), '\n')
 
     T_start = timeit.default_timer()
 
     # create the quadTrees and collect sizes (width, height) of the cropped images of the words
-    word_Trees, word_img_size = createQuadTrees(words, sizes)
+    createQuadTrees(normalTokens)
 
     T_stop = timeit.default_timer()
     print('(i)  QuadTrees have been made for all words in', T_stop - T_start, 'seconds.','\n')
 
     #2. We now find places for the words on our canvas
 
-    c_W, c_H = 4000, 4000
+    #c_W, c_H = 4000, 4000
     #c_W, c_H = 2000, 1200
-    #c_W, c_H = 3000, 1500
+    c_W, c_H = 3000, 1500
 
 
     print('(ii) Now trying to place the words.\n')
@@ -259,17 +262,15 @@ def placeWords(words, sizes):
     #3a. we start with the 1st word
 
 
-    places = [ ]  #returned value; list of tuples representing places of words: if no suggested place, we put NONE
-    ups_and_downs = [ random.randint(0,20)%2  for i in range( len(words) )]
+    ups_and_downs = [ random.randint(0,20)%2  for i in range( len(normalTokens) )]
 
     strLog = '' # the log file
 
-    for i, word in enumerate(words):
-
-        print( word , end = ' ' )
+    for i, token in enumerate(normalTokens):
+        print( token.word , end = ' ' )
         sys.stdout.flush()  # force the output to display what is in the buffer
 
-        strLog_word = '<' + word + '>\n'
+        strLog_word = '<' + token.word + '>\n'
 
         a = 0.1 #the parameter of the spiral
         place_found = False
@@ -286,7 +287,7 @@ def placeWords(words, sizes):
         if i == 0:
             w, h =  np.random.randint( int(0.3*c_W), int(0.5*c_W) ),   int(0.5*c_H) + np.random.randint( -50,50 )
         else:
-            if sizes[i] < 0.3*max( sizes ):
+            if token.fontSize < 0.3*FONT_SIZE_MAX:
                 w, h =  np.random.randint( int(0.3*c_W), int(0.7*c_W) ),   int(0.5*c_H) + np.random.randint( -50,50 )
             else:
                 w, h =  np.random.randint( int(0.3*c_W), int(0.7*c_W) ),   int(0.5*c_H) + np.random.randint( -50,50 )
@@ -300,7 +301,7 @@ def placeWords(words, sizes):
             A = SP.Rectangular(2, ups_and_downs[i]).generator
 
         dx0, dy0 = 0, 0
-        place1 = (w,h)
+        place1 = (w, h)
 
         word_img_path.append( (w,h) )
 
@@ -336,61 +337,26 @@ def placeWords(words, sizes):
 
             if last_hit_index < i:
                 j = last_hit_index
-                if places[j] is not None:
-                    if BBox.collisionTest(word_Trees[i], word_Trees[j], place1, places[j], STAY_AWAY) == True:
+                if normalTokens[j].place != None:
+                    if BBox.collisionTest( token.quadTree, normalTokens[j].quadTree, place1, normalTokens[j].place, STAY_AWAY) == True:
                         collision = True
 
             if collision == False:
-                for j in range( i ): #check for collisions with the rest
-                    if ((j != last_hit_index) and (places[j] is not None)):
-                        if BBox.collisionTest(word_Trees[i], word_Trees[j], place1, places[j], STAY_AWAY) == True:
+                for j in range( i ): # check for collisions with the rest
+                    if ((j != last_hit_index) and (normalTokens[j].place != None)):
+                        if BBox.collisionTest(token.quadTree, normalTokens[j].quadTree, place1, normalTokens[j].place, STAY_AWAY) == True:
                             collision = True
                             last_hit_index = j
                             break
 
             if collision == False:
-                if BBox.insideCanvas( word_Trees[i] , place1, (c_W, c_H) ) == True:
-                    places.append( place1 )
+                if BBox.insideCanvas( token.quadTree , place1, (c_W, c_H) ) == True:
+                    token.place = place1
                     place_found = True
                     strLog_word += '    Place was found\n'
                     break # breaks the move in the Archimedian spiral
-                else:
-                    no_collision_place.append(  place1  )
 
 
-
-
-        if place_found == False:
-            print('no place was found')
-
-            strLog_word += '    Number of collision_free_outside canvas places =' + str(len(no_collision_place)) +'\n'
-
-            if not no_collision_place:
-                places.append(None)
-            else:
-                if  i == 0 : # the first word
-                    places.append( no_collision_place[0]  )
-                else:
-                    # print('BUT there is some outside the canvas')
-
-                    #k1, k2 = int(0.5*c_W), int(0.5*c_H) #the centre of the canvas
-                    k1, k2 =  int(0.5*(places[0][0] + word_img_size[0][0])), int(0.5*(places[0][1] + word_img_size[0][1]))
-
-                    place1 = no_collision_place[0]
-                    c_x, c_y = int(0.5*(place1[0] + word_img_size[i][0])),  int(0.5*(place1[1] + word_img_size[i][1]))
-                    ds = (k1 - c_x)**2 + (k2 - c_y)**2
-                    d_index = 0
-                    for p in range(1, len(no_collision_place)):
-                        place1 = no_collision_place[p]
-                        c_x, c_y = int(0.5*(place1[0] + word_img_size[i][0])),  int(0.5*(place1[1] + word_img_size[i][1]))
-                        ds_1 = (k1 - c_x)**2 + (k2 - c_y)**2
-                        if ds_1<ds:
-                            ds, d_index = ds_1 , p
-
-                    places.append( no_collision_place[d_index] )
-                    #NOTE!!! - - -  - - - TODO -  - - - - - -- -
-                    #if the no collision place has negative (x or y), we need
-                    #to to replace the origin of the canvas to accommodate those words
 
 
 
@@ -403,7 +369,7 @@ def placeWords(words, sizes):
     #with open('LogFile.txt', 'w+') as f:
     #    f.write(strLog)
 
-    return [ (c_W, c_H),  places , word_img_size, word_img_path ]
+    return c_W, c_H
 
 
 
@@ -421,11 +387,10 @@ def createWordle_fromFile( fName ):
         print( str(s) +  (7-len(str(s)))*' ' + ':  ' + tokens[i]  )
 
 
-    #N_of_words = 150
-    tokens, freq =  normalizeWordSize(tokens, freq, TOKENS_TO_USE, FONT_SIZE_MAX, FONT_SIZE_MIN)
-    finalPlaces = placeWords(tokens, freq)
+    normalTokens =  normalizeWordSize(tokens, freq, TOKENS_TO_USE, FONT_SIZE_MAX, FONT_SIZE_MIN)
+    canvas_W, canvas_H = placeWords(normalTokens)
 
-    wordle = drawOnCanvas(tokens, freq, finalPlaces)
+    wordle = drawOnCanvas(normalTokens, (canvas_W, canvas_H ) )
 
     wordle.save( fName[0:-4] + '_wordle.png')
 
