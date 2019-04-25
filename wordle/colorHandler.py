2f8459f00aa9141b702ae3e3d0b28e1fd6e15a80
hayka <hayk.aleksanyan@gmail.com>
2019-04-25 17:52:55 +0400

    allowing random selection of color schemes

--- wordle/colorHandler.py
+++ wordle/colorHandler.py
@@ -38,41 +38,78 @@ def hslToRgb(h, s, l):
 
     return int(r * 255), int(g * 255), int(b * 255)
 
-
-def getRandomColor():
+def getRandomColor(h = -1, s = -1, l = -1):
     """
         return a random RGB triple which is not very bright as a color
+        we allow for some of the components of the HSL be predefined, and others being chosen at random
+        to fix the specific HSL coordinate change the -1 value to the one you wish from interval [0,1]
 
-        Note! bright colors need high saturation and high luminosity
-        Note! lower saturation means grayish colors
+        Note ==>  bright colors need high saturation and high luminosity
+        Note ==>  lower saturation means grayish colors
     """
 
-    h = random.random()
-    s = random.random()
+    if h == -1 or not 0 <= h <= 1:
+        h = random.random()
+    if s == -1 or not 0 <= s <= 1:
+        s = random.random()
+        if s > 0.8:
+            s = 0.5
+    if l == -1 or not 0 <= l <= 1:
+        l = random.random()
+        if l > 0.5:
+            l = 1 - l
 
-    if s > 0.8:
-        s = 0.5
-    l = random.random()
-    if l > 0.5:
-        l = 1 - l
-        #l = max(1 - l, 0.4)
-        #l = 0.5
+    return hslToRgb(h, s, l)
 
-    # s = 1
-    # l = 0.5
-    # s = 0
 
-    return hslToRgb(h,s,l)
 
 def colorTokens(normalTokens):
-    # given a list of Tokens (see the wordle for Token class) we add their color attributes
+    """
+      given a list of Tokens (see the wordle for Token class) we add their color attributes
+      by picking a random color scheme and applying on the Tokens
+    """
 
+    scheme = random.randint(0, 3)
+
+    if scheme == 0:
+        print('\nColor scheme: Random\n')
+        randomColors(normalTokens)
+    elif scheme == 1:
+        print('\nColor scheme: Jet\n')
+        jetColors(normalTokens)
+    elif scheme == 2:
+        print('\nColor scheme: Grayish\n')
+        grayishRandomColors(normalTokens)
+    elif scheme == 3:
+        print('\nColor scheme: From a fixed list\n')
+        chooseFromFixedSchemes(normalTokens)
+
+
+
+def randomColors(normalTokens):
+    # apply random colors of slightly darker gamma
     for token in normalTokens:
         token.color = getRandomColor()
 
+def jetColors(normalTokens):
+    # apply jet color (heatmap) scheme on tokens
+    if not normalTokens:  return
+
+    h, s, l = 0, 0.85, 0.4
+    step = 0.7/len(normalTokens)
+
+    for token in normalTokens:
+        token.color = hslToRgb(h, s, l)
+        h += step
+
+def grayishRandomColors(normalTokens):
+    # apply grayscale random colors
+    for token in normalTokens:
+        # get a random color with lower saturation, this will ensure a grayish color
+        token.color = getRandomColor(h = -1, s = 0, l = -1)
 
 def chooseFromFixedSchemes(normalTokens):
-    # chose a color scheme based on the word frequency (fontSize)
+    # choose a color scheme from a fixed list which colors tokens based on the fontSize
 
     color_schemes = [
         [ (89, 97, 113), (115, 124, 140), (141, 150, 168), (179, 188, 204), (219, 227, 240) ],
